56b1528b8faf2f9a2b62a63f46ad012e5530bc02
Henry Adagala <adagalahenry@gmail.com>
2019-01-25 14:52:12 +0300

    Delete firebase.ts

--- src/environments/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBxif8Rna0apz842lR_YI_06BMbKu9rX94',
-  authDomain: 'todo-app-be578.firebaseapp.com',
-  databaseURL: 'https://todo-app-be578.firebaseio.com',
-  projectId: 'todo-app-be578',
-  storageBucket: 'todo-app-be578.appspot.com',
-  messagingSenderId: '520582923793'
-};
